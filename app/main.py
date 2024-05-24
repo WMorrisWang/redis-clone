572a1936ae752c274586682a9541ea69c40cd7cf
Morris <pupugggg@DESKTOP-U37ML91>
2024-05-24 20:38:52 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        connection,address = server_socket.accept()
-        connection.send(bytes('+PONG\r\n','utf-8'))
+    with socket.create_server(("localhost", 6379), reuse_port=True) as serverSocket:
+        connection,address = serverSocket.accept()
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.send(bytes('+PONG\r\n','utf-8'))
 
 if __name__ == "__main__":
     main()
