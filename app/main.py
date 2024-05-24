c08474ce4bf9613a6d92e1689941e61cf0e486f2
Morris <pupugggg@DESKTOP-U37ML91>
2024-05-24 19:06:35 +0800

    response to ping command for a single connection

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
-
+    while True:
+        connection,address = server_socket.accept()
+        connection.send(bytes('+PONG\r\n','utf-8'))
 
 if __name__ == "__main__":
     main()
